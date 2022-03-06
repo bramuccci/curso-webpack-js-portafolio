f5eb5942e69cbf6afd568e293be2f13174cde826
Candela Bramucci <candelabramucci05@gmail.com>
2022-03-06 16:22:40 -0300

    commit one

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import Template from './templates/Template.js';
-console.log('hola');
-
-(async function App() {
-  const main = null || document.getElementById('main');
-  main.innerHTML = await Template();
-})();
+import Template from '@templates/Template.js'
+import '@styles/main.css'
+;(async function App() {
+    const main = null || document.getElementById('main')
+    main.innerHTML = await Template()
+})()
